d328e10539dda6487e6e0a9150439c9156da411b
Anmol-Tripathi2004 <149133533+Anmol-Tripathi2004@users.noreply.github.com>
2025-02-19 21:23:43 +0530

    Add files via upload

---
+++ src/controllers/ai.controller.js
@@ -0,0 +1,10 @@
+const aiServices=require("../services/ai.service");
+
+module.exports.getReview= async(req,res)=>{
+    const code=req.body.code;
+    if(!code){
+        return res.status(400).send("prompt is required");
+    }
+    const response=await aiServices(code);
+    res.send(response);
+}
